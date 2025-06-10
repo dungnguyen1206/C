bd430bceceeef79a0881e02383124c00a9b446b6
Nguyễn Việt Dũng <dungnasan126@gmail.com>
2025-06-10 15:55:35 +0700

    Update and rename Q10.c to Problem_14.c

--- Workshop5/Problem_14.c
+++ Workshop5/Problem_14.c
@@ -29,26 +29,7 @@ int main() {
   char c[9];
   fgets(c,sizeof(c),stdin);
   hambu2(c);
-//  int i;int d=0;
-//  for(i=strlen(c)-1;i>=0;i--){
-//  	if(c[i]=='1'){
-//  		d=i; 
-//		break;
-//	  }
-//  }
-//  if(d){
-//  for(i=d-1;i>=0;i--){
-//  	if(c[i]=='0'){
-//  		c[i]='1';
-//	  }
-//    else if(c[i]=='1'){
-//		c[i]='0';
-//	}
-//  }
-//  }
-//  
-  
-  
+
   
   // Fixed Do not edit anything here.
   printf("\nOUTPUT:\n");
