34e6eb18fc0e86e6f5bf01c3e930c5702e01d762
Nguyễn Việt Dũng <dungnasan126@gmail.com>
2025-06-10 15:54:42 +0700

    Add files via upload

---
+++ Workshop5/Q10.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+void hambu2(char c[]){
+	int i;int d=0;
+  for(i=strlen(c)-1;i>=0;i--){
+  	if(c[i]=='1'){
+  		d=i; 
+		break;
+	  }
+  }
+  if(d){
+  for(i=d-1;i>=0;i--){
+  	if(c[i]=='0'){
+  		c[i]='1';
+	  }
+    else if(c[i]=='1'){
+		c[i]='0';
+	}
+  }
+  }
+}
+
+int main() {
+  system("cls");
+  //INPUT - @STUDENT:ADD YOUR CODE FOR INPUT HERE:
+  char c[9];
+  fgets(c,sizeof(c),stdin);
+  hambu2(c);
+//  int i;int d=0;
+//  for(i=strlen(c)-1;i>=0;i--){
+//  	if(c[i]=='1'){
+//  		d=i; 
+//		break;
+//	  }
+//  }
+//  if(d){
+//  for(i=d-1;i>=0;i--){
+//  	if(c[i]=='0'){
+//  		c[i]='1';
+//	  }
+//    else if(c[i]=='1'){
+//		c[i]='0';
+//	}
+//  }
+//  }
+//  
+  
+  
+  
+  // Fixed Do not edit anything here.
+  printf("\nOUTPUT:\n");
+  //@STUDENT: WRITE YOUR OUTPUT HERE:
+  printf("%s",c);
+  
+  
+  
+  
+  
+  
+  //--FIXED PART - DO NOT EDIT ANY THINGS HERE
+  printf("\n");
+  system ("pause");
+  return(0);
+}
